a24ebc8161a72222037b66d40b06360221e387cb
keevbush <keevbush@gmail.com>
2019-05-24 13:32:54 +0200

    [add] ajout des configurations du système

---
+++ WorkInApi/Services/CosmoDbConfig.cs
@@ -0,0 +1,23 @@
+﻿using MvcControlsToolkit.Business.DocumentDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkInApi.Services
+{
+    public class CosmoDbConfig
+    {
+        private static IDocumentDBConnection _instance = null;
+        public static IDocumentDBConnection Instance
+        {
+            get
+            {
+                if(_instance==null)
+                    _instance= new DefaultDocumentDBConnection("https://localhost:8081", 
+                        "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", "workin");
+                return _instance;
+            }
+        }
+    }
+}
---
+++ WorkInApi/Services/IDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace WorkInApi.Services
+{
+    public interface IDataAccess<T> where T: class
+    {
+        IEnumerable<T> GetAllItem();
+        void NewItems(params T[] items);
+        void UpdateItem(string id, T item);
+        void DeleteItem(string id);
+        IEnumerable<T> GetItems(Expression<Func<T, bool>> where);
+        
+    }
+}
