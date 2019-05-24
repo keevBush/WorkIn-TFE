9547cc7043616f1437aa798c7856f8f0da027ba8
keevbush <keevbush@gmail.com>
2019-05-24 13:30:40 +0200

    [add] les acces a la base donnees pour les collection entreprises et demandeurs

---
+++ WorkInApi/DAL/EntrepriseCollection.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using WorkInApi.Models;
+using WorkInApi.Services;
+
+namespace WorkInApi.DAL
+{
+    public class EntrepriseCollection : IDataAccess<Entreprise>
+    {
+        public void DeleteItem(string id)
+        {
+            CosmoDbConfig.Instance
+                    .Client
+                    .DeleteDocumentAsync(UriFactory.
+                    CreateDocumentUri(CosmoDbConfig.Instance.DatabaseId, "entreprises", id));
+        }
+
+        public IEnumerable<Entreprise> GetAllItem()
+        {
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri(CosmoDbConfig.Instance.DatabaseId, "entreprises");
+            FeedOptions feedOptions = new FeedOptions { MaxItemCount = -1 };
+            IDocumentQuery<Entreprise> demandeurs = CosmoDbConfig.Instance.Client.
+                    CreateDocumentQuery<Entreprise>(collectionUri, feedOptions)
+                    .AsDocumentQuery();
+            List<Entreprise> listofEntreprise = new List<Entreprise>();
+            while (demandeurs.HasMoreResults)
+                listofEntreprise.AddRange(demandeurs.ExecuteNextAsync<Entreprise>().Result);
+            return listofEntreprise;
+        }
+
+        public IEnumerable<Entreprise> GetItems(Expression<Func<Entreprise, bool>> where)
+        {
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri(CosmoDbConfig.Instance.DatabaseId, "entreprises");
+            FeedOptions feedOptions = new FeedOptions { MaxItemCount = -1 };
+            IDocumentQuery<Entreprise> demandeurs;
+            if (where == null)
+                demandeurs = CosmoDbConfig.Instance.Client.
+                    CreateDocumentQuery<Entreprise>(collectionUri, feedOptions)
+                    .AsDocumentQuery();
+            else
+                demandeurs = CosmoDbConfig.Instance.Client.
+                    CreateDocumentQuery<Entreprise>(collectionUri, feedOptions)
+                    .Where(where)
+                    .AsDocumentQuery();
+            List<Entreprise> listofEntreprise = new List<Entreprise>();
+            while (demandeurs.HasMoreResults)
+                listofEntreprise.AddRange(demandeurs.ExecuteNextAsync<Entreprise>().Result);
+            return listofEntreprise;
+        }
+
+        public void NewItems(params Entreprise[] items)
+        {
+            foreach (var i in items)
+                CosmoDbConfig.Instance
+                    .Client
+                    .CreateDocumentAsync(UriFactory
+                    .CreateDocumentCollectionUri(CosmoDbConfig.Instance.DatabaseId, "entreprises"),
+                    i);
+        }
+
+        public void UpdateItem(string id, Entreprise item)
+        {
+            if (string.IsNullOrEmpty(id.Trim()))
+                throw new InvalidOperationException("Id is not specified");
+            if (id != item.Id)
+                throw new InvalidOperationException("Les id ne correspondent pas");
+            Uri documentUri = UriFactory.CreateDocumentUri(CosmoDbConfig.Instance.DatabaseId, "entreprises", id);
+            CosmoDbConfig.Instance
+                    .Client
+                    .ReplaceDocumentAsync(documentUri, item);
+        }
+    }
+}
---
+++ WorkInApi/DAL/UserCollection.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using WorkInApi.Models;
+using WorkInApi.Services;
+
+namespace WorkInApi.DAL
+{
+    public class UserCollection : IDataAccess<Demandeur>
+    {
+        public void DeleteItem(string id)
+        {
+            CosmoDbConfig.Instance
+                    .Client
+                    .DeleteDocumentAsync(UriFactory.
+                    CreateDocumentUri(CosmoDbConfig.Instance.DatabaseId, "demandeurs", id));
+        }
+
+        public IEnumerable<Demandeur> GetAllItem()
+        {
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri(CosmoDbConfig.Instance.DatabaseId, "demandeurs");
+            FeedOptions feedOptions = new FeedOptions { MaxItemCount = -1 };
+            IDocumentQuery<Demandeur> demandeurs = CosmoDbConfig.Instance.Client.
+                    CreateDocumentQuery<Demandeur>(collectionUri, feedOptions)
+                    .AsDocumentQuery();
+            List<Demandeur> listofDemandeur = new List<Demandeur>();
+            while (demandeurs.HasMoreResults)
+                listofDemandeur.AddRange(demandeurs.ExecuteNextAsync<Demandeur>().Result);
+            return listofDemandeur;
+
+        }
+
+        public IEnumerable<Demandeur> GetItems(Expression<Func<Demandeur, bool>> where)
+        {
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri(CosmoDbConfig.Instance.DatabaseId, "demandeurs");
+            FeedOptions feedOptions = new FeedOptions { MaxItemCount = -1 };
+            IDocumentQuery<Demandeur> demandeurs;
+            if (where == null)
+                demandeurs  = CosmoDbConfig.Instance.Client.
+                    CreateDocumentQuery<Demandeur>(collectionUri, feedOptions)
+                    .AsDocumentQuery();
+            else
+                demandeurs = CosmoDbConfig.Instance.Client.
+                    CreateDocumentQuery<Demandeur>(collectionUri, feedOptions)
+                    .Where(where)
+                    .AsDocumentQuery();
+            List<Demandeur> listofDemandeur = new List<Demandeur>();
+            while (demandeurs.HasMoreResults)
+                listofDemandeur.AddRange(demandeurs.ExecuteNextAsync<Demandeur>().Result);
+            return listofDemandeur;
+        }
+
+        public void NewItems(params Demandeur[] items)
+        {
+            foreach (var i in items)
+                CosmoDbConfig.Instance
+                    .Client
+                    .CreateDocumentAsync(UriFactory
+                    .CreateDocumentCollectionUri(CosmoDbConfig.Instance.DatabaseId, "demandeurs"),
+                    i);
+        }
+
+        public void UpdateItem(string id, Demandeur item)
+        {
+            if (string.IsNullOrEmpty(id))
+                throw new ApplicationException("No demandeur id specified");
+            Uri documentUri = UriFactory.CreateDocumentUri(CosmoDbConfig.Instance.DatabaseId, "demandeurs", id);
+            CosmoDbConfig.Instance
+                    .Client
+                    .ReplaceDocumentAsync(documentUri, item);
+        }
+    }
+}
