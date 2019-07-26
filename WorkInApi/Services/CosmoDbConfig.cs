6da336041e8518aad52543383c92d4f5ed4e747f
keevbush <keevbush@gmail.com>
2019-07-26 21:29:43 +0200

    [fix ] deploiement du service

--- WorkInApi/Services/CosmoDbConfig.cs
+++ WorkInApi/Services/CosmoDbConfig.cs
@@ -14,8 +14,8 @@ namespace WorkInApi.Services
             get
             {
                 if(_instance==null)
-                    _instance= new DefaultDocumentDBConnection("https://localhost:8081",
-                        "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", "workin");
+                    _instance= new DefaultDocumentDBConnection("https://bushffe.documents.azure.com:443/",
+                        "qGwmco6RolIxOEoKw8UQYtn4e7gznkt9KWIN60Bv5zFAaLBPc2VFrPcud1Oq1or2qxj27wlvjeZPRLnLefSzcA==", "workin");
                 return _instance;
             }
         }
